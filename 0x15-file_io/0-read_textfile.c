6cd6b88aa602ab4f907add98eddd7494f2c50d82
Benfati20 <benalifati20@gmail.com>
2023-05-08 08:02:02 -0700

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,58 +1,39 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - read the contents of a text file//
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * @filename: filename of the file to be read////
- * @letters: number of letters (characters to be read and printed)
- *
- * Return: number of letters read and printed--//
- * 0 - if file could not be opened or read
- * 0 - if file name is NULL
- * 0 - if write failed or write did not write required bytes
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *read_str;
-	ssize_t open_ret_val;
-	ssize_t read_ret_val;
-	ssize_t write_ret_val;
+	ssize_t o, r, w;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-        if (letters == 0)
-                return (0);
 
-	open_ret_val = open(filename, O_RDONLY);
-	if (open_ret_val == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	read_str = malloc(letters);
-	read_ret_val = read(open_ret_val, read_str, letters);
-	if (read_ret_val == -1)
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(read_str);
-		close(open_ret_val);
+		free(buffer);
 		return (0);
 	}
 
-	write_ret_val = write(STDOUT_FILENO, read_str, read_ret_val);
-	if (write_ret_val == -1)
-	{
-		free(read_str);
-		close(open_ret_val);
-		return (0);
-	}
-	if (write_ret_val != read_ret_val)
-	{
-		free(read_str);
-		close(open_ret_val);
-		return (0);
-	}
+	free(buffer);
+	close(o);
 
-	free(read_str);
-	close(open_ret_val);
-
-	return (write_ret_val);
+	return (w);
 }
